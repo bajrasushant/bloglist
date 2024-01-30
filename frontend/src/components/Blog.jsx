4ba2d7e6e20a88a7f85a5581734a25772faf1c75
Sushant Bajracharya <bajra_sushant@gmail.com>
2024-01-30 16:55:46 +0545

    need to remove togglable and find user who posted the blog

--- frontend/src/components/Blog.jsx
+++ frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
 
-export default Blog
+  const [visible, setVisible] = useState(false);
+
+  const hideWhenVisible = { display: visible ? "none" : "" };
+  const showWhenVisible = { display: visible ? "" : "none" };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+      
+        <button style={hideWhenVisible} onClick={toggleVisibility}>view</button>
+      <div style={showWhenVisible}>
+          <div>{blog.url}</div>
+          <div>likes {blog.likes}</div>
+        <button onClick={toggleVisibility}>hide</button>
+      </div>
+      </div>
+    </div>
+  );
+};
+export default Blog;
